8f3e03a945acbc53e0fcb467aa6fc166d9a9fd87
jbredwards <julian.edwards@maine.edu>
2023-09-18 14:43:49 -0400

    set indexOf parameter to a Fraction

--- myArrayList.java
+++ myArrayList.java
@@ -5,11 +5,7 @@ public class myArrayList extends ArrayList<Fraction>
     protected static final int INDEX_NOT_FOUND = -1;
     public myArrayList() { ensureCapacity(50); }
 
-    @Override
-    public int indexOf(Object input) {
-        if(input == null) { for(int i = 0; i < size(); i++) if(get(i) == null) return i; }
-        else for(int i = 0; i < size(); i++) if(input.equals(i)) return i;
-        // not found
-        return INDEX_NOT_FOUND;
+    public int indexOf(Fraction input) {
+        return super.indexOf(input);
     }
 }
