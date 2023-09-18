308bdef5c045723234bae25541e3127fc7c53f16
jbredwards <julian.edwards@maine.edu>
2023-09-18 14:47:20 -0400

    remove unused INDEX_NOT_FOUND

--- myArrayList.java
+++ myArrayList.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 
 public class myArrayList extends ArrayList<Fraction>
 {
-    protected static final int INDEX_NOT_FOUND = -1;
     public myArrayList() { ensureCapacity(50); }
 
     public int indexOf(Fraction input) {
