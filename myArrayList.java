d7883a31830fca5c58db4310ab388bf16ec3c032
silasqualls <144641458+silasqualls@users.noreply.github.com>
2023-09-24 15:35:50 -0400

    Add files via upload

---
+++ myArrayList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+//Code By Silas
+public class myArrayList extends ArrayList<Fraction> {
+
+    private static final int INITIAL_CAPACITY = 50;
+    private int capacity;
+
+    public myArrayList() {
+        super(INITIAL_CAPACITY); // Initialize with the initial capacity of 50
+        this.capacity = INITIAL_CAPACITY;
+    }
+
+
+    public void add(int index, Fraction input) {
+        if (index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
+        }
+        ensureCapacity(size() + 1);
+        super.add(index, input);
+    }
+}
